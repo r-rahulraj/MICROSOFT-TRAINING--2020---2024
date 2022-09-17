c542fe8384788c4adf112560e01f37d4bb6a9af7
r-rahulraj <105582691+r-rahulraj@users.noreply.github.com>
2022-09-17 14:50:23 +0530

    Update 14. PerfectSquare.java

--- GOOGLE ASSIGNMENTS/ASSIGNMENT 1/14. PerfectSquare.java
+++ GOOGLE ASSIGNMENTS/ASSIGNMENT 1/14. PerfectSquare.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		long fact=1;
-		
-		for(int i=n;i>=1;i--) {
-		    fact*=i;
-		}
-		System.out.print(fact);
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if(isPerfectSquare(n)) System.out.print("Perfect Square");
+	    else System.out.print("Not a Perfect Square");
+	}
+	public static boolean isPerfectSquare(int n) {
+	    if(n==1) return true;
+	    for(int i=1;i<=n/2;i++) {
+	        if(i*i==n) return true;
+	    }
+	    return false;
 	}
 }
