240a502e056a24c74ac868358f18049b6f49f96b
r-rahulraj <105582691+r-rahulraj@users.noreply.github.com>
2022-09-17 14:32:57 +0530

    Create 5. Area of circle.java

---
+++ GOOGLE ASSIGNMENTS/ASSIGNMENT 1/5. Area of circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double r=sc.nextInt();
+		double area=3.14*r*r;
+    
+		System.out.print(area);
+	}
+}
