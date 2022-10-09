e59ac1eeea60ebc8c3e19da5d0d06ab2a0251751
r-rahulraj <105582691+r-rahulraj@users.noreply.github.com>
2022-10-09 16:21:19 +0530

    Create sumOfSubarrayRanges.java

---
+++ Microsoft/HomeWork-ASSIGNMENTS/Week3/sumOfSubarrayRanges.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+	    int n = nums.length;
+	    long res = 0;
+	    for (int i = 0; i < n; i++) {
+		    int min = nums[i], max = nums[i];
+		    for(int j = i; j < n; j++) {
+			    min = Math.min(min, nums[j]);
+			    max = Math.max(max, nums[j]);
+			    res += max - min;
+		    }
+	    }
+	    return res;
+    }
+}
