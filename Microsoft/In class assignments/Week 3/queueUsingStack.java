02b49cff0aa1407f3a410f9f2ce9a6ca8a820ad5
r-rahulraj <105582691+r-rahulraj@users.noreply.github.com>
2022-10-09 16:11:52 +0530

    Create queueUsingStack.java

---
+++ Microsoft/In class assignments/Week 3/queueUsingStack.java
@@ -0,0 +1,27 @@
+class MyQueue {
+    Stack<Integer> queue = new Stack<Integer>();
+    
+    public void push(int x) {
+        Stack<Integer> temp = new Stack<Integer>();
+        while(!queue.empty()){
+            temp.push(queue.pop());
+        }   
+        queue.push(x);
+        while(!temp.empty()){
+            queue.push(temp.pop());
+        }
+    }
+
+    public int pop() {
+        return queue.pop();
+    }
+
+    public int peek() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.empty();
+    }
+}
+
